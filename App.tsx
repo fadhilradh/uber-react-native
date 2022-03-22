4dfca07807337904c81ed10f45ebb9fa5e9e9de9
fadhilradh <fadhilradhian@gmail.com>
2022-03-23 05:27:35 +0700

    ADD redux and tailwind

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import HomeScreen from "./screens/HomeScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <Provider store={store}>
+      <HomeScreen />
+    </Provider>
   );
 }
 
---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import React from "react";
+import tw from "twrnc";
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView style={tw``}>
+      <Text style={tw`text-red-500`}>HomeScreen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
